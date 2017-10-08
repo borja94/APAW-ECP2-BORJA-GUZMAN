455a7da7a3c6acb06d36d9d5765ac78b8d24abbf
borja <borja@Borja>
2017-10-08 19:36:48 +0200

    Elimino la referencia a las clases de test funcionales hasta que tengamos test

--- src/test/java/es/upm/miw/apaw/ecp2/borja/guzman/AllSubjectsFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/ecp2/borja/guzman/AllSubjectsFunctionalTesting.java
@@ -1,14 +1,7 @@
 package es.upm.miw.apaw.ecp2.borja.guzman;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({
-    SubjectResourceFunctionalTesting.class, 
-    StudentResourceFunctionalTesting.class
-})
+
 public class AllSubjectsFunctionalTesting {
 
 }

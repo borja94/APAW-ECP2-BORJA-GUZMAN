08ff4f2c3c11af670bd6e92865465da65b464254
borja <borja@Borja>
2017-10-08 19:33:25 +0200

    Añado las clases para los test funcionales

---
+++ src/test/java/es/upm/miw/apaw/ecp2/borja/guzman/AllSubjectsFunctionalTesting.java
@@ -0,0 +1,14 @@
+package es.upm.miw.apaw.ecp2.borja.guzman;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    SubjectResourceFunctionalTesting.class, 
+    StudentResourceFunctionalTesting.class
+})
+public class AllSubjectsFunctionalTesting {
+
+}

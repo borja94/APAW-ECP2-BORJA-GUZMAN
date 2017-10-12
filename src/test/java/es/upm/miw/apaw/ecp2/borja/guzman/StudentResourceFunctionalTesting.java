a6533f2ba95c8c452f08927b93a3b0a6f434c270
borja <borja@Borja>
2017-10-12 13:56:40 +0200

    Creación de los test necesarios para la issue#04

--- src/test/java/es/upm/miw/apaw/ecp2/borja/guzman/StudentResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/ecp2/borja/guzman/StudentResourceFunctionalTesting.java
@@ -1,5 +1,47 @@
 package es.upm.miw.apaw.ecp2.borja.guzman;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import es.upm.miw.apaw.ecp2.borja.guzman.http.HttpException;
+
+
 public class StudentResourceFunctionalTesting {
 
+	@Before
+	public void before() {
+
+	}
+
+	private void createStudent() {
+		
+	}
+
+	@Test
+	public void testCreateStudent() {
+
+		this.createStudent();
+	}
+	
+	@Test(expected= HttpException.class)
+	public void testCreateStudentNameEmpty() {
+		throw new HttpException("test");
+	}
+	
+	@Test(expected= HttpException.class)
+	public void testCreateStudentDNIEmpty() {
+		throw new HttpException("test");
+	}
+	
+	@Test(expected= HttpException.class)
+	public void testCreateStudentBirthDateEmpty() {
+		throw new HttpException("test");
+	}
+	
+	@Test(expected= HttpException.class)
+	public void testCreateStudentRepeated() {
+		this.createStudent();
+		this.createStudent();
+		throw new HttpException("test");
+	}
 }
